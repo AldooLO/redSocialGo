1ee5db0e6b89e6a119e11b8887c7396837d8245f
aldo <aldo_lo@outlook.com>
2025-06-05 11:47:48 -0600

    Ultimo Commit

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -4,7 +4,8 @@ import (
 	"context"
 	"fmt"
 	"redSocialGo/models"
-
+	"redSocialGo/routers"
+	"redSocialGo/jwt"
 	"github.com/aws/aws-lambda-go/events"
 )
 
@@ -13,9 +14,19 @@ func Manejadores(ctx context.Context, request events.APIGatewayProxyRequest) mod
 
 	var r models.RespApi
 	r.Status = 400
+
+	isOk, statusCode, msg, _ := validoAuthorization(ctx, request)
+	if !isOk{
+		r.Status = statusCode
+		r.Message = msg
+		return r
+	}
+
 	switch ctx.Value(models.Key("method")).(string){
 	case "POST":
 		switch ctx.Value(models.Key("path")).(string){
+		case "registro":
+			return routers.Registro(ctx)
 		}
 	case "GET":
 		switch ctx.Value(models.Key("path")).(string){
@@ -30,4 +41,31 @@ func Manejadores(ctx context.Context, request events.APIGatewayProxyRequest) mod
 
 	r.Message = "Method Invalid"
 	return r
+}
+
+func validoAuthorization(ctx context.Context, request events.APIGatewayProxyRequest) (bool, int, string, models.Claim){
+	path := ctx.Value(models.Key("path")).(string)
+
+	if path == "registro" || path == "login" || path == "obtenerAvatar" || path == "obtenerBanner" {
+		return true, 200, "", models.Claim{}
+	}
+
+	token := request.Headers["Authorization"]
+	if len(token) == 0 {
+		return false, 401, "Token requerido", models.Claim{}
+	}
+
+	claim, todoOk, msg, err := jwt.ProcesoToken(token, ctx.Value(models.Key("jwtSing")).(string))
+	if !todoOk {
+		if err != nil{
+			fmt.Println("Error en el token " + err.Error())
+			return false, 401, err.Error(), models.Claim{}
+		}else{
+			fmt.Println("Error en el token " + msg)
+			return false, 401, msg, models.Claim{}
+		}
+	}
+
+	fmt.Println("Token Ok")
+	return true, 200, msg, *claim
 }
---
+++ jwt/token.go
@@ -0,0 +1,39 @@
+package jwt
+
+import (
+	"errors"
+	"go/token"
+	"redSocialGo/bd"
+	"redSocialGo/models"
+	"strings"
+
+	jwt "github.com/golang-jwt/jwt/v5"
+)
+
+var Email string
+var IDUsuario string
+
+func ProcesoToken(tk string, JWTSing string) (*models.Claim, bool, string, error){
+	miClave := []byte(JWTSing)
+	var claims models.Claim
+
+	splitToken := strings.Split(tk, "Bearer")
+	if len(splitToken) != 2 {
+		return &claims, false, string(""), errors.New("formato de token inválido")
+	}
+
+	tk = strings.TrimSpace(splitToken[1])
+
+	tkn, err := jwt.ParseWithClaims(tk, &claims, func(token *jwt.Token) (interface{}, error){
+		return miClave, nil
+	})
+
+	if err == nil {
+
+	}
+
+	if !tkn.Valid{
+		return &claims, false, string(""), errors.New("Token Inválido")
+	}
+	return &claims, false, string(""), err
+}
