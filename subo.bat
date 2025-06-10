f3cd79186c93839465497ed6364b4d45922693f4
aldo <aldo_lo@outlook.com>
2025-06-10 11:57:25 -0600

    Ultimo Commit

--- subo.bat
+++ subo.bat
@@ -2,5 +2,5 @@ git add .
 git commit -m "Ultimo Commit"
 git push
 GOOS=linux CGO_ENABLED=0 GOARCH=arm64 go buil -tags lambda.norpc -o bootstrap main.go
-del main.zip
+del go_lambda.zip
 zip go_lambda.zip bootstrap
